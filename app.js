76fdc1edd7bcb40d2c1917002881c41dec62be4e
Your Name <rsmith5901@gmail.com>
2017-06-17 18:46:59 -0400

    working

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-const isProd = false;
+const isProd = true;
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
