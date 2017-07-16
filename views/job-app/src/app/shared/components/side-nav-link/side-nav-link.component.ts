78a4855566871a897948c55011bd3443dd5809ef
Your Name <rsmith5901@gmail.com>
2017-07-16 13:08:49 -0400

    working

--- views/job-app/src/app/shared/components/side-nav-link/side-nav-link.component.ts
+++ views/job-app/src/app/shared/components/side-nav-link/side-nav-link.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SideNavLinkComponent implements OnInit {
   @Input() isSideBarOpen: boolean;
+  @Input() linkText: string;
+  @Input() linkPath: string;
 
   constructor() { }
 
