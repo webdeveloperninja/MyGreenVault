c848a2adf05c17ea62da12e0016f603e5b029ec2
Your Name <rsmith5901@gmail.com>
2017-07-29 14:08:38 -0400

    redirect

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,7 @@ exports.postLogin = (req, res, next) => {
     req.logIn(user, (err) => {
       if (err) { return next(err); }
       req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect(req.session.returnTo || '/');
+      res.redirect('/');
     });
   })(req, res, next);
 };
