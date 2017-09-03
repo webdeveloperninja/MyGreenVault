9a67a544ea5e081e650e646ebf331ff74932b261
Your Name <rsmith5901@gmail.com>
2017-09-02 21:57:38 -0400

    working

--- views/job-app/src/app/tools/components/checkout-tool/checkout-tool.component.ts
+++ views/job-app/src/app/tools/components/checkout-tool/checkout-tool.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {Jsonp, URLSearchParams} from '@angular/http';
+
+
+@Injectable()
+export class WikipediaService {
+  constructor(private _jsonp: Jsonp) {}
+
+  search(term: string) {
+    if (term === '') {
+      return Observable.of([]);
+    }
+
+    let wikiUrl = 'https://en.wikipedia.org/w/api.php';
+    let params = new URLSearchParams();
+    params.set('search', term);
+    params.set('action', 'opensearch');
+    params.set('format', 'json');
+    params.set('callback', 'JSONP_CALLBACK');
+
+    return this._jsonp
+      .get(wikiUrl, {search: params})
+      .map(response => <string[]> response.json()[1]);
+  }
+}
+
 
 @Component({
   selector: 'ti-checkout-tool',
   templateUrl: './checkout-tool.component.html',
-  styleUrls: ['./checkout-tool.component.scss']
+  styleUrls: ['./checkout-tool.component.scss'],
+  providers: [WikipediaService]
 })
 export class CheckoutToolComponent implements OnInit {
 
   @Input() tool: any;
 
-  constructor() { }
+  model: any;
+  searching = false;
+  searchFailed = false;
+  hideSearchingWhenUnsubscribed = new Observable(() => () => this.searching = false);
+
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .do(() => this.searching = true)
+      .switchMap(term =>
+        this._service.search(term)
+          .do(() => this.searchFailed = false)
+          .catch(() => {
+            this.searchFailed = true;
+            return Observable.of([]);
+          }))
+      .do(() => this.searching = false)
+      .merge(this.hideSearchingWhenUnsubscribed);
+
+  constructor(private _service: WikipediaService) {}
 
   ngOnInit() {
   }
