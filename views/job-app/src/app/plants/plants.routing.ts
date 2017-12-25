4c84486fd5a80a4fcfcdf05e28342830f8287f48
Your Name <rsmith5901@gmail.com>
2017-12-25 11:49:32 -0500

    working

--- views/job-app/src/app/plants/plants.routing.ts
+++ views/job-app/src/app/plants/plants.routing.ts
@@ -4,7 +4,7 @@ import { KanbanComponent } from './components/kanban/kanban.component';
 import { JobDetailComponent } from './components/job-detail/job-detail.component';
 
 export const routes: Routes = [
-    // { path: 'jobs', component: JobsComponent },
+    { path: 'jobs', component: JobsComponent },
     // { path: 'jobs/:jobNumber',      component: JobDetailComponent },
     { path: 'kanban', component: KanbanComponent }
 ];
