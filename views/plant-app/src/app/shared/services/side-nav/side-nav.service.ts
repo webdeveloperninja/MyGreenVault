d5c5b9f4fd199f58cdafcc13745a7ff2c20f30a7
Your Name <rsmith5901@gmail.com>
2018-07-01 12:51:58 -0400

    asfd

--- views/plant-app/src/app/shared/services/side-nav/side-nav.service.ts
+++ views/plant-app/src/app/shared/services/side-nav/side-nav.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+
 import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable()
 export class SideNavService {
-
   private _isSideNavOpen$: BehaviorSubject<boolean> = new BehaviorSubject<any>(false);
   public readonly isSideNavOpen$: Observable<boolean> = this._isSideNavOpen$.asObservable();
 
-  constructor(
-    private _localStorageService: LocalStorageService
-  ) { 
+  constructor(private _localStorageService: LocalStorageService) {
     let isOpen = this._localStorageService.getItem('isSideBarOpen');
     if (isOpen) {
       this._isSideNavOpen$.next(isOpen.isSideBarOpen);
@@ -19,13 +17,12 @@ export class SideNavService {
   }
 
   openSideNav() {
-    this._localStorageService.setItem('isSideBarOpen', JSON.stringify({isSideBarOpen: true}));
+    this._localStorageService.setItem('isSideBarOpen', JSON.stringify({ isSideBarOpen: true }));
     this._isSideNavOpen$.next(true);
   }
 
   shutSideNav() {
-    this._localStorageService.setItem('isSideBarOpen', JSON.stringify({isSideBarOpen: false}));
+    this._localStorageService.setItem('isSideBarOpen', JSON.stringify({ isSideBarOpen: false }));
     this._isSideNavOpen$.next(false);
   }
-
 }
