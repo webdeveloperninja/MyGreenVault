9c1e7698f9098e3e12b041dca595499d5b9b3b16
Your Name <rsmith5901@gmail.com>
2018-07-21 18:53:57 -0400

    initial actions

--- angular-plant-app/src/app/plant-details/containers/details/details.component.ts
+++ angular-plant-app/src/app/plant-details/containers/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as fromDetails from '../../reducers/plant-details.reducer';
+import * as fromDetailsActions from '../../actions/details.actions';
+
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'vault-details',
@@ -6,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  constructor() {}
+  routeParameters$ = this._activatedRoute.params;
+  plantId: string;
+  constructor(private readonly _activatedRoute: ActivatedRoute, private _store: Store<fromDetails.State>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.routeParameters$.subscribe(routeParameters => {
+      this.plantId = routeParameters.id;
+      this.getPlantDetails(this.plantId);
+    });
+  }
+
+  getPlantDetails(plantId: string) {
+    this._store.dispatch(new fromDetailsActions.TestAction());
+  }
 }
--- angular-plant-app/src/app/plant-details/plant-details-routing.module.ts
+++ angular-plant-app/src/app/plant-details/plant-details-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailsComponent } from './containers/details/details.component';
 
-export const routes: Routes = [{ path: 'details', component: DetailsComponent }];
+export const routes: Routes = [{ path: 'details/:id', component: DetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
